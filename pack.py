5388d0f9b6e32af332edecdda114959038aa69d2
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-04-23 11:18:53 +0200

    Test all bitdepths [1, 16]

--- pack.py
+++ pack.py
@@ -14,17 +14,16 @@ def pack(x, count):
 def unpack(buf, count):
     bits = np.unpackbits(buf, bitorder="little")
     bits = bits.reshape((-1, count))
-    bits = np.pad(bits, ((0, 0), (0, 16 -count)))
+    bits = np.pad(bits, ((0, 0), (0, 16 - count)))
     return np.packbits(bits, bitorder="little").view("<u2")
 
 
-bitdepth = 12
-width, height = 1920, 1088
-rng = np.random.default_rng()
-image = rng.integers(1 << bitdepth, size=(height, width), dtype=np.uint16)
+for bitdepth in range(1, 17):
+    width, height = 1920, 1088
+    rng = np.random.default_rng()
+    image = rng.integers(1 << bitdepth, size=(height, width), dtype=np.uint16)
 
-encoded = pack(image, count=bitdepth)
-print(f"Compression ratio: {image.nbytes / encoded.nbytes:.3}")
-decoded = unpack(encoded, count=bitdepth).reshape((height, width))
-np.testing.assert_array_equal(image, decoded)
-breakpoint()
+    encoded = pack(image, count=bitdepth)
+    print(f"Compression ratio: {image.nbytes / encoded.nbytes:.3}")
+    decoded = unpack(encoded, count=bitdepth).reshape((height, width))
+    np.testing.assert_array_equal(image, decoded)
