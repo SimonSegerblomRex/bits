9e60dd1612e596eefd72a080ebadc4e5cec199cd
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-04-24 09:59:32 +0200

    Add support for big endian

--- pack.py
+++ pack.py
@@ -3,50 +3,48 @@ import numpy as np
 from bitarray.util import int2ba
 
 
-def pack(x, count):
+def pack(x, bitdepth, bitorder="little"):
     itemsize = x.dtype.itemsize
     bits = np.unpackbits(
         x.astype(f"<u{itemsize}", copy=False).view("u1").reshape(-1, itemsize),
         axis=1,
         bitorder="little",
-        count=count,
+        count=bitdepth,
     )
-    return np.packbits(bits, bitorder="little")
+    if bitorder == "big":
+        bits = bits[:, ::-1]
+    return np.packbits(bits, bitorder=bitorder)
 
 
-def unpack(buf, count):
-    bits = np.unpackbits(buf, bitorder="little").reshape(-1, count)
+def unpack(buf, bitdepth, bitorder="little"):
+    bits = np.unpackbits(buf, bitorder=bitorder).reshape(-1, bitdepth)
+    if bitorder == "big":
+        bits = bits[:, ::-1]
     bytes_ = np.packbits(bits, axis=1, bitorder="little")
-    itemsize = np.min_scalar_type((1 << count) - 1).itemsize
+    itemsize = np.min_scalar_type((1 << bitdepth) - 1).itemsize
     if bytes_.shape[1] < itemsize:
         bytes_ = np.pad(bytes_, ((0, 0), (0, itemsize - bytes_.shape[1])))
     return bytes_.reshape(-1).view(f"<u{itemsize}")
 
 
+for bitorder in ["little", "big"]:
+    print(f"Bitorder: {bitorder}")
+    for bitdepth in range(1, 65):
+        width, height = 1920, 1088
+        rng = np.random.default_rng()
+        itemsize = np.min_scalar_type((1 << bitdepth) - 1).itemsize
+        image = rng.integers(1 << bitdepth, size=(height, width), dtype=f"<u{itemsize}")
 
-for bitdepth in range(1, 65):
-    width, height = 16, 16#1920, 1088
-    rng = np.random.default_rng()
-    itemsize = np.min_scalar_type((1 << bitdepth) - 1).itemsize
-    image = rng.integers(1 << bitdepth, size=(height, width), dtype=f"<u{itemsize}")
+        encoded = pack(image, bitdepth=bitdepth, bitorder=bitorder)
+        print(f"Compression ratio: {image.nbytes / encoded.nbytes:5.2f} (bitdepth: {bitdepth:2})")
 
-    encoded = pack(image, count=bitdepth)
-    print(f"Compression ratio: {image.nbytes / encoded.nbytes:5.2f} (bitdepth: {bitdepth:2})")
+        if 0:
+            # bitarray module
+            ba = bitarray.bitarray(endian=bitorder)
+            code = {i: int2ba(i, length=bitdepth, endian=bitorder) for i in range(1 << bitdepth)}
+            ba.encode(code, image.ravel())
+            baout = ba.tobytes()
+            assert baout == encoded.tobytes()
 
-    # bitarray module
-    ba = bitarray.bitarray(endian="little")
-    code = {i: int2ba(i, length=bitdepth, endian="little") for i in range(1 << bitdepth)}
-    ba.encode(code, image.ravel())
-    baout = ba.tobytes()
-    assert baout == encoded.tobytes()
-
-    decoded = unpack(encoded, count=bitdepth).reshape((height, width))
-    np.testing.assert_array_equal(image, decoded)
-
-
-# TODO:
-# MSB
-# >>> np.unpackbits(np.array([3], dtype="<u2").view("u1"), bitorder="little")
-# array([1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=uint8)
-# >>> np.unpackbits(np.array([3], dtype=">u2").view("u1"), bitorder="big")
-# array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], dtype=uint8)
+        decoded = unpack(encoded, bitdepth=bitdepth, bitorder=bitorder).reshape((height, width))
+        np.testing.assert_array_equal(image, decoded)
